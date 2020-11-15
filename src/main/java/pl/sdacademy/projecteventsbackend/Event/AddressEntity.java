44eb3b9720cefc5f60f99902db9739c666902bca
Kika94 <marta.sojka94gmail.com>
2020-11-15 14:35:29 +0100

    Adress

---
+++ src/main/java/pl/sdacademy/projecteventsbackend/Event/AddressEntity.java
@@ -0,0 +1,43 @@
+package pl.sdacademy.projecteventsbackend.Event;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class AddressEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String street;
+    private String city;
+    private int zipcode;
+
+    public AddressEntity() {
+    }
+
+    public AddressEntity(long id, String street, String city, int zipcode) {
+        this.id = id;
+        this.street = street;
+        this.city = city;
+        this.zipcode = zipcode;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+}

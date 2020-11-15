64c77e25bfa0e7cac36e6f1d01835a9e3090e705
Leszek Czechak <leszekczechak@gmail.com>
2020-11-15 19:51:13 +0100

    UserResponse - getters and setters added

--- src/main/java/pl/sdacademy/projecteventsbackend/user/dto/UserResponse.java
+++ src/main/java/pl/sdacademy/projecteventsbackend/user/dto/UserResponse.java
@@ -10,4 +10,19 @@ public class UserResponse {
         this.mail = mail;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
 }

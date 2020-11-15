a1e73e082e8e45747a303bb4afe7e37860d74a9d
MariuszWy <mariusz.wytrych@gmail.com>
2020-11-15 15:46:29 +0100

    added EventEntity

--- src/main/java/pl/sdacademy/projecteventsbackend/Event/EventEntity.java
+++ src/main/java/pl/sdacademy/projecteventsbackend/Event/EventEntity.java
@@ -1,4 +1,55 @@
 package pl.sdacademy.projecteventsbackend.Event;
 
+import pl.sdacademy.projecteventsbackend.User.UserEntity;
+
+import javax.persistence.*;
+
+@Entity
 public class EventEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    @ManyToOne
+    private AddressEntity address;
+
+
+    public EventEntity(String name, String description, AddressEntity address) {
+        this.name = name;
+        this.description = description;
+        this.address = address;
+    }
+
+    public EventEntity() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public AddressEntity getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAddress(AddressEntity address) {
+        this.address = address;
+    }
 }

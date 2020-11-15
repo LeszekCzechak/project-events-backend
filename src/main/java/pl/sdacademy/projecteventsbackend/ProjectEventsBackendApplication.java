ad49dc23569efa99c22fb94c4875bbf6a8cb7aaa
d.zarzycki <d.zarzycki@sdacademy.pl>
2020-11-15 12:34:51 +0100

    test commit

--- src/main/java/pl/sdacademy/projecteventsbackend/ProjectEventsBackendApplication.java
+++ src/main/java/pl/sdacademy/projecteventsbackend/ProjectEventsBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectEventsBackendApplication {
-
+    //test
     public static void main(String[] args) {
         SpringApplication.run(ProjectEventsBackendApplication.class, args);
     }

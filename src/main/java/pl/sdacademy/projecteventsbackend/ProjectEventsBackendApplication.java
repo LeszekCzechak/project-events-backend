db8ab5c03b46ef67ad11d25787b8f7a93f9b416d
MariuszWy <mariusz.wytrych@gmail.com>
2020-11-15 11:39:32 +0100

    project initialization

---
+++ src/main/java/pl/sdacademy/projecteventsbackend/ProjectEventsBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.projecteventsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEventsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEventsBackendApplication.class, args);
+    }
+
+}

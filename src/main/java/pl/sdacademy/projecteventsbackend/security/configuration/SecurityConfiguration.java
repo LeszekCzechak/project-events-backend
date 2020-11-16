13437d0734b8a8b54bbaa69f640a04b1ec5fdaa7
Leszek Czechak <leszekczechak@gmail.com>
2020-11-16 18:49:58 +0100

    SecurityConfiguration: - configure(AuthenticationManagerBuilder) added - configure(HttpSecurity) .cors().disable() added

--- src/main/java/pl/sdacademy/projecteventsbackend/security/configuration/SecurityConfiguration.java
+++ src/main/java/pl/sdacademy/projecteventsbackend/security/configuration/SecurityConfiguration.java
@@ -1,13 +1,21 @@
 package pl.sdacademy.projecteventsbackend.security.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import pl.sdacademy.projecteventsbackend.user.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private  final UserService userService;
+
+    public SecurityConfiguration(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -16,8 +24,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/","/user/register").permitAll()
-                .anyRequest().permitAll(); // TODO: Change permission
+                .anyRequest().permitAll().and().cors().disable(); // TODO: Change permission
+    }
 
-
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
     }
 }

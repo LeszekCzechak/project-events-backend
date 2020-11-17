05a696cd2ac3ad83ff41b4d7ad0769d5596f5fc3
Robert <robertworek01@gmail.com>
2020-11-18 00:03:38 +0100

    Create CommentEntity

--- src/main/java/pl/sdacademy/projecteventsbackend/comment/CommentEntity.java
+++ src/main/java/pl/sdacademy/projecteventsbackend/comment/CommentEntity.java
@@ -1,18 +1,24 @@
 package pl.sdacademy.projecteventsbackend.comment;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import pl.sdacademy.projecteventsbackend.event.EventEntity;
+import pl.sdacademy.projecteventsbackend.user.model.UserEntity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class CommentEntity {
 
+    //user name, user id, event id, event name, content (nullable or „0” by default) - LocalDateTime createdOn; - LocalDateTime updatedOn;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String event;
-    private String user;
+    @ManyToOne
+    private EventEntity eventId;
+    @ManyToOne
+    private UserEntity name;
+    private LocalDateTime createdOn;
+    private LocalDateTime updateOn;
     private String content;
 
 
@@ -20,11 +26,11 @@ public class CommentEntity {
 
     }
 
-    public CommentEntity(Long id, String event, String user, String content) {
+    public CommentEntity(Long id, EventEntity eventId, UserEntity name) {
         this.id = id;
-        this.event = event;
-        this.user = user;
-        this.content = content;
+        this.eventId = eventId;
+        this.name = name;
+        this.content = null;
     }
 
     public Long getId() {
@@ -35,20 +41,36 @@ public class CommentEntity {
         this.id = id;
     }
 
-    public String getEvent() {
-        return event;
+    public EventEntity getEventId() {
+        return eventId;
     }
 
-    public void setEvent(String event) {
-        this.event = event;
+    public void setEventId(EventEntity eventId) {
+        this.eventId = eventId;
     }
 
-    public String getUser() {
-        return user;
+    public UserEntity getName() {
+        return name;
     }
 
-    public void setUser(String user) {
-        this.user = user;
+    public void setName(UserEntity name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public LocalDateTime getUpdateOn() {
+        return updateOn;
+    }
+
+    public void setUpdateOn(LocalDateTime updateOn) {
+        this.updateOn = updateOn;
     }
 
     public String getContent() {
